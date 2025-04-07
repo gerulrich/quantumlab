f8b26d8e0d242a8e224085baa803eaef76025b87
German Ulrich <gerulrich@gmail.com>
2025-04-07 00:48:46 -0300

    docs: add Cilium + API Gateway installation guide
    Includes steps to patch the CNI configuration, disable kube-proxy, install Gateway API CRDs, and deploy Cilium with gateway support.
    References config files under infrastructure/network/cilium/.

---
+++ scripts/quanum-env.sh
@@ -0,0 +1,5 @@
+# set up environment variables talosctl and kubectl
+export KUBECONFIG=$PWD/talos/kubeconfig
+export TALOSCONFIG=$PWD/talos/talosconfig
+export CONTROL_PLANE_IP=192.168.0.125
+export WORKER_IP=192.168.0.230

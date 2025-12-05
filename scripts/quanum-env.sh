49721be0c5045f4be9c6f168a81b908b6646e7f3
German Ulrich <gerulrich@gmail.com>
2025-12-05 00:42:50 -0300

    chore: update environment variables for quantum-talos configuration

--- scripts/quanum-env.sh
+++ scripts/quanum-env.sh
@@ -1,6 +1,6 @@
 # set up environment variables talosctl and kubectl
-export KUBECONFIG=$PWD/talos/kubeconfig
-export TALOSCONFIG=$PWD/talos/talosconfig
-export CONTROL_PLANE_IP=192.168.0.125
-export WORKER_IP=192.168.0.230
+export KUBECONFIG=$PWD/config/quantum-talos/kubeconfig
+export TALOSCONFIG=$PWD/config/quantum-talos/talosconfig
+export CONTROL_PLANE_IP=10.10.10.194
+export WORKER_IP=10.10.10.173
 export SOPS_AGE_KEY_FILE=$PWD/age.key

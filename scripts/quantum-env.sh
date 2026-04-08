b9151031909662befcd6df609c82df5724bcd6ad
German Ulrich <gerulrich@gmail.com>
2026-04-07 23:34:02 -0300

    chore(deps): update FluxCD version to v2.8.5 and Tailscale operator version to v1.96.5

--- scripts/quantum-env.sh
+++ scripts/quantum-env.sh
@@ -3,7 +3,7 @@
 # Versions
 export TALOS_VERSION="1.12.6"
 export KUBERNETES_VERSION="1.35.3"
-export FLUX_VERSION="2.8.3"
+export FLUX_VERSION="2.8.5"
 export CILIUM_VERSION="1.19.2"
 export SCHEMATIC_ID=a2e824fa8b6d72b70f9076cebd483a76cd56a07a0a81372611a8ed6fe3b6b95e
 

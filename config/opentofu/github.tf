5c18b738552f38fdf96aeb76228ee9a81d9e0575
German Ulrich <gerulrich@gmail.com>
2026-06-03 00:51:58 -0300

    chore: update GitHub Actions secrets to use 'value' instead of 'plaintext_value' (#202)

--- config/opentofu/github.tf
+++ config/opentofu/github.tf
@@ -20,33 +20,33 @@ resource "github_actions_secret" "dockerhub_username" {
   for_each            = data.github_repository.repos
   repository          = each.value.name
   secret_name         = "DOCKERHUB_USERNAME"
-  plaintext_value     = var.github_secrets["DOCKERHUB_USERNAME"].value
+  value               = var.github_secrets["DOCKERHUB_USERNAME"].value
 }
 
 resource "github_actions_secret" "dockerhub_token" {
   for_each            = data.github_repository.repos
   repository          = each.value.name
   secret_name         = "DOCKERHUB_TOKEN"
-  plaintext_value     = var.github_secrets["DOCKERHUB_TOKEN"].value
+  value               = var.github_secrets["DOCKERHUB_TOKEN"].value
 }
 
 resource "github_actions_secret" "webhook_url" {
   for_each            = data.github_repository.repos
   repository          = each.value.name
   secret_name         = "WEBHOOK_URL"
-  plaintext_value     = var.github_secrets["WEBHOOK_URL"].value
+  value               = var.github_secrets["WEBHOOK_URL"].value
 }
 
 resource "github_actions_secret" "ts_oauth_client_id" {
   for_each            = data.github_repository.repos
   repository          = each.value.name
   secret_name         = "TS_OAUTH_CLIENT_ID"
-  plaintext_value     = var.github_secrets["TS_OAUTH_CLIENT_ID"].value
+  value               = var.github_secrets["TS_OAUTH_CLIENT_ID"].value
 }
 
 resource "github_actions_secret" "ts_oauth_secret" {
   for_each            = data.github_repository.repos
   repository          = each.value.name
   secret_name         = "TS_OAUTH_SECRET"
-  plaintext_value     = var.github_secrets["TS_OAUTH_SECRET"].value
+  value               = var.github_secrets["TS_OAUTH_SECRET"].value
 }

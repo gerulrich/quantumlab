3eb99216631a46f5521c44a357a66c37a4648628
German Ulrich <gerulrich@gmail.com>
2026-06-26 00:22:38 -0300

    chore(deps): update terraform oci to ~> 8.20.0 (#219)

--- config/opentofu/providers.tf
+++ config/opentofu/providers.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 8.19.0"
+      version = "~> 8.20.0"
     }
     tailscale = {
       source  = "tailscale/tailscale"

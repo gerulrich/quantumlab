4a1ec664677262cf719747dd6888cf1b18632512
German Ulrich <gerulrich@gmail.com>
2026-08-07 23:33:52 -0300

    chore(env): remove FLUX_VERSION and KUBERNETES_VERSION variables from quantum-env.sh (#270)
    * chore(env): remove FLUX_VERSION variable from quantum-env.sh
    
    * remove unused FLUX_VERSION variable from upgrade-versions.sh
    
    * remove unused KUBERNETES_VERSION variable from quantum-env.sh and upgrade-versions.sh

--- scripts/upgrade-versions.sh
+++ scripts/upgrade-versions.sh
@@ -23,8 +23,6 @@ rm -f "$ENV_FILE.bak"
 # Get latest Kubernetes version
 KUBERNETES_VERSION=$(curl -s https://api.github.com/repos/kubernetes/kubernetes/releases/latest | jq -r .tag_name | sed 's/^v//')
 echo "Kubernetes version: $KUBERNETES_VERSION"
-sed -i.bak "s/export KUBERNETES_VERSION=\".*\"/export KUBERNETES_VERSION=\"$KUBERNETES_VERSION\"/" "$ENV_FILE"
-rm -f "$ENV_FILE.bak"
 KUBERNETES_NEXT_PATCH=$(echo "$KUBERNETES_VERSION" | awk -F. '{print $1"."$2"."$3+1}')
 sed -E -i.bak "s/(upgrade-k8s --to )[0-9]+\.[0-9]+\.[0-9]+/\1${KUBERNETES_NEXT_PATCH}/g" "docs/talos.md"
 rm -f "docs/talos.md.bak"
@@ -76,8 +74,6 @@ fi
 # Get latest Flux version
 FLUX_VERSION=$(curl -s https://api.github.com/repos/fluxcd/flux2/releases/latest | jq -r .tag_name | sed 's/^v//')
 echo "Flux version: $FLUX_VERSION"
-sed -i.bak "s/export FLUX_VERSION=\".*\"/export FLUX_VERSION=\"$FLUX_VERSION\"/" "$ENV_FILE"
-rm -f "$ENV_FILE.bak"
 
 # Get latest Cilium version
 CILIUM_VERSION=$(curl -s https://api.github.com/repos/cilium/cilium/releases/latest | jq -r .tag_name | sed 's/^v//')

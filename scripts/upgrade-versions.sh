a044a0e46c5d76cd94519b810f85befe8154c217
German Ulrich <gerulrich@gmail.com>
2026-08-29 00:53:19 -0300

    chore: clean up upgrade-versions.sh by removing Tailscale version updates add separate versioning rules for Longhorn package

--- scripts/upgrade-versions.sh
+++ scripts/upgrade-versions.sh
@@ -4,7 +4,6 @@
 ENV_FILE="scripts/quantum-env.sh"
 README_FILE="README.md"
 API_GATEWAY_DOC="docs/setup/cilium-api-gateway.md"
-TAILSCALE_RELEASE_FILE="helm/tailscale-operator/release.yaml"
 
 # Helper function to confirm changes
 confirm() {
@@ -88,16 +87,6 @@ echo "Gateway API version: $API_GATEWAY_VERSION"
 # Update Gateway API version in documentation
 sed -E -i.bak "s#(gateway-api/releases/download/v)[0-9]+\.[0-9]+\.[0-9]+#\1${API_GATEWAY_VERSION}#" "$API_GATEWAY_DOC"
 rm -f "$API_GATEWAY_DOC.bak"
-
-# Get latest Tailscale Operator version
-TAILSCALE_VERSION=$(curl -s https://api.github.com/repos/tailscale/tailscale/releases/latest | jq -r .tag_name | sed 's/^v//')
-echo "Tailscale Operator version: $TAILSCALE_VERSION"
-
-# Update Tailscale Operator chart version in HelmRelease
-TAILSCALE_CHART_VERSION=$(curl -s https://pkgs.tailscale.com/helmcharts/index.yaml | awk '/^[[:space:]]+tailscale-operator:/{in_chart=1; next} in_chart && /^[[:space:]]+version:[[:space:]]/{print $2; exit}')
-echo "Tailscale chart version: $TAILSCALE_CHART_VERSION"
-sed -E -i.bak "s#(^[[:space:]]*version:[[:space:]]*\")[0-9]+\.[0-9]+\.[0-9]+(\"$)#\1${TAILSCALE_CHART_VERSION}\2#" "$TAILSCALE_RELEASE_FILE"
-rm -f "$TAILSCALE_RELEASE_FILE.bak"
 
 # Update README version badges
 sed -E -i.bak "s#(badge/Kubernetes-v)[0-9]+\.[0-9]+\.[0-9]+#\1${KUBERNETES_VERSION}#" "$README_FILE"

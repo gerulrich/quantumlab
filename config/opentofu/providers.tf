eea59faf50a25428ecd6b6cac001f466391df9a8
German Ulrich <gerulrich@gmail.com>
2026-09-21 22:25:09 -0300

    chore(deps): update terraform proxmox to v0.114.0 (#428)

--- config/opentofu/providers.tf
+++ config/opentofu/providers.tf
@@ -29,7 +29,7 @@ terraform {
     }
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.113.1"
+      version = "0.114.0"
     }
     github = {
       source  = "integrations/github"
